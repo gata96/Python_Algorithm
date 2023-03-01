d9950dc7e830e5632230acf1b7953825f2960e36
Gata <121420427+gata96@users.noreply.github.com>
2023-03-02 07:40:18 +0900

    Update 피시방 알바.py

--- 백준/Bronze/1453. 피시방 알바/피시방 알바.py
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.py
@@ -1,8 +1,3 @@
-import sys
-input = sys.stdin.readline
-
-cnt = int(input())
-num1 = list(map(int, input().split()))
-num2 = set(num1)
-
-print(len(num1)-len(num2))
+n = int(input())
+ppl = map(int, input().split())
+print(n-len(set(ppl)))
