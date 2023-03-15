bee751f376a06f67d8dad844fe3eac650edb82aa
Gata <121420427+gata96@users.noreply.github.com>
2023-03-15 10:41:44 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
@@ -1,10 +1,3 @@
 def solution(hp):
-    A = hp // 5
-    a = hp % 5
-    B = a // 3
-    b = a % 3
-    C = b // 1
-    c = b % 1
-    return A+B+C
-    
+    return hp // 5 + (hp % 5 // 3) + ((hp % 5) % 3)
     
